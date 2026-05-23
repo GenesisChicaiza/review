69cd7210d0392e9c43e510c725978af8b9995ae0
Genesis Ainhoa Chicaiza Toasa <genesisainhoa.chicaizatoasa@usp.ceu.es>
2026-05-23 23:24:13 +0200

    test 1 falla

---
+++ test_analizador.py
@@ -0,0 +1,19 @@
+import unittest
+
+from src.analizador import AnalizadorTexto
+
+
+class TestContarLineas(unittest.TestCase):
+
+    def test_cuenta_lineas_no_vacias(self):
+        texto = "hola mundo\n\nbuenos dias\n   \nadios\n"
+        a = AnalizadorTexto(texto)
+        self.assertEqual(a.contar_lineas(), 3)
+
+    def test_texto_vacio_tiene_cero_lineas(self):
+        self.assertEqual(AnalizadorTexto("").contar_lineas(), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
